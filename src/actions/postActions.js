33a287771afa9eda1c50094ffaeacca8ba48b08c
Caleb Spain <crspain@uh.edu>
2020-02-21 23:34:31 -0600

    Updated Post Actions

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POST } from './types'
+import { FETCH_POSTS } from './types'
 
 export const fetchPosts = () => dispatch => {
     fetch('https://jsonplaceholder.typicode.com/posts')
